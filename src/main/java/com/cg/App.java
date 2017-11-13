6f57c30a0b15b4c6c7d0da0c16f491cdcac4d23f
nikhilkhatavkar <nikhilkhatavkar@gmail.com>
2017-11-13 12:17:30 +0530

    first Commmit

---
+++ src/main/java/com/cg/App.java
@@ -0,0 +1,17 @@
+package com.cg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+        try{System.out.println( "Hello World!" );
+		}
+		catch(Exception e){}
+	
+	}
+}
